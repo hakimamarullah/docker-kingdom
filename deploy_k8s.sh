27300daaba50ff2187dfb7b080a304bb4e154e2e
hakimamarullah <hakimamarullah@gmail.com>
2024-09-28 14:31:45 +0700

    Setup rabbit:

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -8,6 +8,7 @@ echo "Starting deployment...."
 kubectl apply -f k8s/components.yaml
 kubectl apply -f k8s/ingress-controller.yaml
 kubectl apply -f k8s/postgres
+kubectl apply -f k8s/rabbitmq
 kubectl apply -f k8s/auth
 kubectl apply -f k8s/api_key_management
 kubectl apply -f k8s/payment_service
