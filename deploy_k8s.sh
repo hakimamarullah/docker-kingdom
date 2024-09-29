087955977a09d438d6747a5938d8ca54d7e90702
hakimamarullah <hakimamarullah@gmail.com>
2024-09-29 14:25:34 +0700

    All good

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -9,10 +9,13 @@ kubectl apply -f k8s/components.yaml
 kubectl apply -f k8s/ingress-controller.yaml
 kubectl apply -f k8s/postgres
 kubectl apply -f k8s/rabbitmq
+kubectl apply -f k8s/github-secret.yaml
+kubectl apply -f k8s/starline_scc
 kubectl apply -f k8s/auth
 kubectl apply -f k8s/api_key_management
 kubectl apply -f k8s/payment_service
 kubectl apply -f k8s/api_wilayah
 kubectl apply -f k8s/frontend
+kubectl apply -f k8s/mailer
 
 echo "All Component Has Been Created"
--- shutdown.sh
+++ shutdown.sh
@@ -7,11 +7,15 @@ echo "Shutting down..."
 
 kubectl delete -f k8s/components.yaml
 kubectl delete -f k8s/ingress-controller.yaml
-kubectl delete -f k8s/postgres
+kubectl delete -f k8s/postgres/postgres-service.yaml
+kubectl delete -f k8s/postgres/postgres-set.yaml
 kubectl delete -f k8s/auth
 kubectl delete -f k8s/api_key_management
 kubectl delete -f k8s/payment_service
 kubectl delete -f k8s/api_wilayah
 kubectl delete -f k8s/ingress.yaml
+kubectl delete -f k8s/github-secret.yaml
+kubectl delete -f k8s/starline_scc
+kubectl delete -f k8s/mailer
 
 echo "All Component Has Been Shutdown"
